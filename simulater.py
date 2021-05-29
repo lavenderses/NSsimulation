c90e4b483ffca635aeb502ab7b735e70f2e13e11
nakanoi <nakanoinryota@gmail.com>
2021-05-29 21:03:07 +0900

    renewed a lot of functions

--- simulater.py
+++ simulater.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anime
 from decimal import Decimal, ROUND_HALF_UP
 from functions import advection_viscosity as adv
 from functions import initialization as ini
@@ -14,8 +16,8 @@ dell         : The Micro Length == The Length of The Micro Volume
 divLx, divLy : Divided Numbers in x, y Directions
 ux, uy       : Fluid Velocities in x, y Directions at Point (x, y)[numpy array]
 vx, vy       : Temporary Velocities in x, y Directions
-ux_calced    : Calculated Velocity in x Direction at Point (x, y)[numpy array]
-uy_calced    : Calculated Velocity in y Direction at Point (x, y)[numpy array]
+ux_ast       : Calculated Velocity in x Direction at Point (x, y)[numpy array]
+uy_ast       : Calculated Velocity in y Direction at Point (x, y)[numpy array]
 div          : Calculated Divergence at Point (x, y)[numpy array](This Must be Nearly Zero)
 p_rho        : Density(Uniform)
 mu           : The Dinamic Viscosity Coeficient
@@ -30,56 +32,61 @@ fx, fy       : The Forces in x, y Directions at Point (x, y)[numpy array]
 '''
 
 
-def simulate(Lx, Ly, H, v0, theta, time_range, delt=0.01, dell=0.1, p_rho=1.2, mu=1.82e-5, eps=1e-8, w=1.8, cnt_max=10000):
+def simulate(Lx, Ly, H, v0, theta, time_range, delt=0.01, dell=0.1, p_rho=1.293, mu=1.82e-5, eps=1e-8, w=1.8, cnt_max=10000):
     #Set Constance Values
     divLx = int(Decimal(str(Lx / dell)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
     divLy = int(Decimal(str(Ly / dell)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
     ux = np.zeros((divLx + 3, divLy + 2))
     uy = np.zeros((divLx + 2, divLy + 3))
-    div = np.zeros((divLx, divLy))
-    P = np.zeros((divLx, divLy))
+    P = np.zeros((divLx + 2, divLy + 2))
+    fig = plt.figure()
     plts = []
-    num = 0
     passed_time = 0
+    theta = np.deg2rad(theta)
+    plt.xlim(-0.1, Lx + 0.1)
+    plt.ylim(-0.1, Ly + 0.1)
+
+    #First Condition
+    ux, uy = ini.force_and_first(ux, uy, delt, H, v0, theta)
 
     #First Plot (must be white)
-    plts = plots.plot(ux, uy, Lx, Ly, delt, dell, dell, plts, v0)
+    plt_img = plots.plot(ux, uy, divLx, divLy, delt, dell, dell, v0, plt, passed_time)
+    plts.append([plt_img])
 
     while passed_time < time_range:
         print(passed_time)
-        num += 1
-
-        #First Condition
-        ux, uy = ini.force_and_first(ux, uy, H, v0, theta)
-
         #Advection
-        ux = adv.advectionX(ux, uy, delt, dell, dell)
-        uy = adv.advectionY(ux, uy, delt, dell, dell)
+        ux_ast = adv.advectionX(ux, uy, delt, dell, dell)
+        uy_ast = adv.advectionY(ux, uy, delt, dell, dell)
 
         #Viscosity
-        ux, uy = adv.viscosity(ux, uy, mu, delt, dell, dell, p_rho)
+        ux_ast, uy_ast = adv.viscosity(ux, uy, ux_ast, uy_ast, delt, dell, dell, mu, p_rho)
 
         #Solce Poisson Eq to Align ux&uy (div u = 0)
-        P = poisson.Poisson(ux, uy, div, delt, dell, dell, P, eps, w, cnt_max)
+        P = poisson.Poisson(ux_ast, uy_ast, delt, dell, dell, P, eps, w, cnt_max)
 
-        #FIX THE VELOCITY
-        ux, uy = poisson.fix_u(ux, uy, P, delt, dell, dell, p_rho)
-        ux, uy = ini.force_and_first(ux, uy, H, v0, theta)
+        #Fix each Velocity
+        ux, uy = poisson.fix_u(ux_ast, uy_ast, P, delt, dell, dell, p_rho)
+        ux, uy = ini.force_and_first(ux, uy, delt, H, v0, theta)
 
-        #PLOT
-        plts = plots.plot(ux, uy, Lx, Ly, delt, dell, dell, plts, v0)
-
+        #Plot
         passed_time += delt
+        plt_img = plots.plot(ux, uy, divLx, divLy, delt, dell, dell, v0, plt, passed_time)
+        plts.append([plt_img])
+
+    ani = anime.ArtistAnimation(fig, plts, interval=100, blit=True)
+    ani.save('./animation.mp4', writer="ffmpeg")
+#    ani.show()
 
 
 if __name__ == '__main__':
-    #SET EACH CONSTANCE
+    #Set Each Constance
     Lx = 4.0
     Ly = 2.5
     H = [[1, 3],
          [1, 4]]
     v0 = 5.0
-    theta = 30
-    time_range = 60.0
+    theta = 0
+    time_range = 20.0
 
     simulate(Lx, Ly, H, v0, theta, time_range)
