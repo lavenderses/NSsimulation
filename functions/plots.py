cda1ef2a2b165faa92c884372b9c270601b45d5c
nakanoi <nakanoinryota@gmail.com>
2021-05-30 01:26:05 +0900

    changed plots

--- functions/plots.py
+++ functions/plots.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 
 def plot(ux, uy, Lx, Ly, delt, delx, dely, v0, plt, t):
-    '''
-    X = np.delete(ux, Lx + 2, 0)
-    Y = np.delete(uy, Ly + 2, 1)
-    X = np.flatten(X)
-    Y = np.flatten(Y)'''
+    X = ux[:Lx+2, :Ly+2]
+    Y = ux[:Ly+2, :Ly+2]
+    X = X.flatten()
+    Y = Y.flatten()
+    fig = plt.figure()
+    #'''
     for x in range(Lx + 2):
         for y in range(Ly + 2):
             v = np.array([ux[x, y], uy[x, y]])
@@ -15,10 +17,17 @@ def plot(ux, uy, Lx, Ly, delt, delx, dely, v0, plt, t):
                       y * dely,
                       ux[x, y] * delt,
                       uy[x, y] * delt,
+                      cmap='jet',
                       color=cm.jet(abs_u / v0),
+                      norm=Normalize(0.0, v0)
                       )
+    '''
+    img = plt.quiver(X, Y, cmap='jet')
+    '''
     plt.title('t = {} [s]'.format(t * 0.1))
-    plt.colorbar(img)
+    fig.colorbar(img)
+    img.set_clim(0.0, v0)
+    plt.plot()
     plt.show()
 
     return img
